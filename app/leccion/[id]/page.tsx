d7a86d47f1d91e48f9acb5e5b76d6aed8d644b24
sqlnova <julietasotelo89@gmail.com>
2026-04-04 00:56:40 -0300

    Cambio en archivo page

--- app/leccion/[id]/page.tsx
+++ app/leccion/[id]/page.tsx
@@ -1,4 +1,9 @@
 'use client'
+
+export function generateStaticParams() {
+  return [0,1,2,3,4,5,6,7,8,9,10].map(id => ({ id: String(id) }))
+}
+
 import { useEffect, useState, useRef, useCallback } from 'react'
 import { useRouter, useParams } from 'next/navigation'
 import { sb } from '@/lib/supabase'
