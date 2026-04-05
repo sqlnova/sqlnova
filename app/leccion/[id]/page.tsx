c725aa89ce5a2b61ee7ae940334be27b383426ec
sqlnova <julietasotelo89@gmail.com>
2026-04-05 17:30:00 -0300

    Update page.tsx

--- app/leccion/[id]/page.tsx
+++ app/leccion/[id]/page.tsx
@@ -1,9 +1,15 @@
 import LeccionClient from './LeccionClient'
 
 export function generateStaticParams() {
-  return [0,1,2,3,4,5,6,7,8,9,10].map(id => ({ id: String(id) }))
+  return Array.from({ length: 11 }, (_, i) => ({ id: String(i) }))
 }
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <LeccionClient moduloId={Number(params.id)} />
+export default async function LeccionPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  const moduloId = parseInt(id)
+  return <LeccionClient moduloId={moduloId} />
 }
